8e9c7476311c3be810f4bd5cfb8b5728a50e0ae3
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2018-10-16 02:03:43 -0300

    Update and rename negative2.cpp to negativo_Q5_p2.cpp

--- negativo_Q5_p2.cpp
+++ negativo_Q5_p2.cpp
@@ -1,4 +1,4 @@
-/*#include <iostream>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
@@ -20,7 +20,7 @@ int main()
 	{
 		cap >> img;
 		Mat new_image = Mat::zeros(img.size(), img.type());
-		img.convertTo(new_image,-1,  -1, 255);
+		img.convertTo(new_image,-1,  -1, 255); //alpha tem valor -1 e o beta 255
 		imshow("Imagem Modificada", new_image);
 		imshow("Imagem Original", img);
 	}
@@ -28,7 +28,7 @@ int main()
 	destroyAllWindows();
 	return 0;
 }
-*/
+
 
 
 
